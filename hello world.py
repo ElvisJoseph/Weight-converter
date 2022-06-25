abbae22a5d47184eb6bf20d2d9c7489d57557f34
ElvisJoseph <71148339+ElvisJoseph@users.noreply.github.com>
2022-06-25 03:47:28 +0300

    Update hello world.py

--- hello world.py
+++ hello world.py
@@ -1,7 +1,7 @@
 weight = int(input("Weight: "))
 unit = input("K(gs) or l(bs):")
 
-if unit == "K":
+if unit.upper == "K":
     converted = weight / 0.45
     print("Weight in lbs:" + str(converted))
 else:
